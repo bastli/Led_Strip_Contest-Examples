a8e9361ee5c516afbccc4ec86dcabd21ca33a6aa
Dominik Schmidt <das1993@hotmail.com>
2015-11-13 22:11:42 +0100

    C: Make Barco_sleep_ms _WIN32-compatible.

--- c/util.c
+++ c/util.c
@@ -1,15 +1,24 @@
 #include "util.h"
-#include <time.h>
-#include <stdlib.h>
+#ifdef _WIN32
+	#include <synchapi.h>
+#else
+	#include <time.h>
+	#include <stdlib.h>
+	#include <unistd.h>
+#endif
 
 RETURN_CODE Barco_sleep_ms(long ms){
-	struct timespec ts;
-	ts.tv_sec=0;
-	ts.tv_nsec=1000*1000;
-	while(ms-->0){
-		if(nanosleep(&ts, NULL)!=0){
-			return RETURN_ERROR;
+	#ifdef _WIN32
+		Sleep(ms);
+	#else
+		struct timespec ts;
+		ts.tv_sec=0;
+		ts.tv_nsec=1000*1000;
+		while(ms-->0){
+			if(nanosleep(&ts, NULL)!=0){
+				return RETURN_ERROR;
+			}
 		}
-	}
+	#endif
 	return RETURN_OK;
 }
