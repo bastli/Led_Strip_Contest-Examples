10be361cd660fe002e15d6c557011de0b09b16a7
joroth <joroth@student.ethz.ch>
2019-12-28 12:41:24 +0100

    add basic flame effect

--- d/main.d
+++ d/main.d
@@ -5,6 +5,7 @@ import std.stdio;
 import std.range;
 import std.algorithm;
 import std.socket;
+import std.random;
 import core.thread;
 
 
@@ -59,11 +60,67 @@ void do_epilepsy(float fs, Color a, Color b=Color.BLACK, float dur=3){
 	}
 }
 
+// define MIN(x,y) ((x) < (y) ? (x) : (y))
+
+Color heat_map(float a){
+	a = min(1, a*3);
+	a *= 0.5;
+	return Color(
+		cast(ubyte)(a*0xff),
+		cast(ubyte)(a*3f*0x3f),
+		cast(ubyte)(a*3f*0x0f));
+}
+
+void do_flame() {
+	float[LED_COUNT+1][STRIP_COUNT] arr;
+	foreach(i; 0..STRIP_COUNT){
+		foreach(ii; 0..LED_COUNT){
+			arr[i][ii] = 0.0f;
+		}
+	}
+	while(1){
+		// calculate heat intensity arr
+		foreach(i; 0..STRIP_COUNT){
+			float val = uniform(0.0f, 1.0f);
+			arr[i][LED_COUNT-1] = val * val * val;
+		}
+
+		// propagate intensity
+		foreach(i; 0..STRIP_COUNT){
+			foreach(ii; 0..LED_COUNT-1){
+				float val = arr[i][ii];
+				val += 0.2f * arr[(i+1)%STRIP_COUNT][ii];
+				val += 0.2f * arr[(i-1+STRIP_COUNT)%STRIP_COUNT][ii];
+				val += 5f * arr[i][ii+1];
+				arr[i][ii] = val * 0.154;// 0.2488f; //0.2286f;
+			}
+		}
+
+		// sparks
+		if(uniform(0f, 1f) < 0.1f){
+
+		}
+
+		// map to strips
+		foreach(i, ref s; sa){
+			//s.set(Color(0x20,0x00,0x00).repeat(LED_COUNT));
+			Color[LED_COUNT] stripe;
+			foreach(ii; 0..LED_COUNT){
+				stripe[ii] = heat_map(arr[i][ii]);
+			}
+			s.set(stripe[]);
+
+		}
+		sock.send(sa);
+		sleep_ms(10);
+	}
+}
+
 BarcoSocket sock;
 StripArray sa;
 void main(string[] args){
 	sa.initialize();
 	sock=new BarcoSocket(new InternetAddress(args[1], STRIP_PORT));
 	//do_epilepsy(10, Color.WHITE*0.05);
-	do_leuchtturm();
+	do_flame();
 }
