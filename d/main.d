2c4ebb19f7d509fb8d6eef87ef8cef4550dd4648
joroth <joroth@student.ethz.ch>
2019-12-29 01:32:08 +0100

    added blackbody radiation

--- d/main.d
+++ d/main.d
@@ -61,6 +61,33 @@ void do_epilepsy(float fs, Color a, Color b=Color.BLACK, float dur=3){
 	}
 }
 
+// l: wavelength
+// t: temperature
+float blackbody_spec(float l, float t){
+	//float a = 1.191043e-16 / pow(l, 5); // 1.191043e-16 = 2 * h * c^2
+	//float b = exp(0.0143877688f/(l*t)); // 0.0143877687 == h * c / k_B
+	float a = 1.191043e-16 / pow(l, 5); // 1.191043e-16 = 2 * h * c^2
+	float b = exp(0.0143877688f/(l*t)); // 0.0143877687 == h * c / k_B
+	return a / (b-1);
+}
+
+Color heat_map_blackbody(float a, float t_max,float max_br=1f){
+	float r = blackbody_spec(650e-9, a*t_max);
+	float g = blackbody_spec(530e-9, a*t_max);
+	float b = blackbody_spec(470e-9, a*t_max);
+	float max = fmax(fmax(r,g),b);
+	return Color(r/max, g/max, b/max) * max_br;
+}
+
+Color heat_map_blackbody_normalize(float t, float max_br=1f){
+	float r = blackbody_spec(650e-9, t);
+	float g = blackbody_spec(530e-9, t);
+	float b = blackbody_spec(470e-9, t);
+	float max = fmax(fmax(r,g),b);
+	return Color(r/max, g/max, b/max) * max_br;
+}
+
+
 Color heat_map_flame(float a, float max_br=1f){
 	a = fmin(1, 3*a);
 
@@ -99,17 +126,17 @@ void do_flame(float max_br=0.3f) {
 	while(1){
 		// calculate heat intensity arr
 		foreach(i; 0..STRIP_COUNT){
-			arr[i][LED_COUNT-1] = 0.5f * arr[i][LED_COUNT-1] + 0.5f * pow(uniform(0f, 1f), 3);
+			arr[i][LED_COUNT-1] = 0.3f * arr[i][LED_COUNT-1] + 0.7f * pow(uniform(0f, 1f), 3);
 		}
 
 		// propagate intensity
 		foreach(i; 0..STRIP_COUNT){
 			foreach(ii; 0..LED_COUNT-1){
-				float val = arr[i][ii];
-				val += 0.2f * arr[(i+1)%STRIP_COUNT][ii];
-				val += 0.2f * arr[(i-1+STRIP_COUNT)%STRIP_COUNT][ii];
-				val += 5f * arr[i][ii+1];
-				arr[i][ii] = val * 0.154; // damping is fideling factor
+				float val = 2f * arr[i][ii];
+				val += 0.05f * arr[(i+1)%STRIP_COUNT][ii];
+				val += 0.05f * arr[(i-1+STRIP_COUNT)%STRIP_COUNT][ii];
+				val += 3f * arr[i][ii+1];
+				arr[i][ii] = val * 0.195;//0.154; // damping is fideling factor
 			}
 		}
 
@@ -142,11 +169,27 @@ void do_flame(float max_br=0.3f) {
 		}
 
 		sock.send(sa);
-		sleep_ms(5);
+		sleep_ms(50);
 	}
 }
 
-void test_flame_map(float max_br=0.3f) {
+void test_stripe_mapping(){
+	foreach(i, ref s; sa){
+		Color[LED_COUNT] stripe;
+		foreach(ii; 0..LED_COUNT){
+			if(ii  <  2 * (i+1)){
+				stripe[ii] = Color.GREEN * 0.3 * ((ii+1)%2);
+			}
+			else{
+				stripe[ii] = Color.BLACK;
+			}
+		}
+		s.set(stripe[]);
+	}
+	sock.send(sa);
+}
+
+void test_map(float max_br=0.3f) {
 	float[LED_COUNT+1][STRIP_COUNT] arr;
 	foreach(i; 0..STRIP_COUNT){
 		foreach(ii; 0..LED_COUNT){
@@ -159,18 +202,12 @@ void test_flame_map(float max_br=0.3f) {
 	foreach(i, ref s; sa){
 		Color[LED_COUNT] stripe;
 		foreach(ii; 0..LED_COUNT){
-			stripe[ii] = heat_map_flame(arr[i][ii], max_br);
-			foreach(ref sp; spark_list){
-				if(i == cast(int)(sp.str) && ii == cast(int)(sp.y)){
-					stripe[ii] = Color(1f,0.5f,0.05f) * 0.2 + heat_map_flame(arr[i][ii], max_br);
-				}
-			}
+			stripe[ii] = heat_map_blackbody(arr[i][ii], (i+1) * 1000f, max_br);
 		}
 		s.set(stripe[]);
 	}
-
 	sock.send(sa);
-	writeln(sa);
+	//writeln(sa);
 }
 
 BarcoSocket sock;
@@ -179,6 +216,9 @@ void main(string[] args){
 	sa.initialize();
 	sock=new BarcoSocket(new InternetAddress(args[1], STRIP_PORT));
 	//do_epilepsy(10, Color.WHITE*0.05);
-	//test_flame_map(0.3);
-	do_flame(0.6);
+	test_map(0.3);
+	//do_leuchtturm();
+	//test_stripe_mapping();
+	//do_flame(0.6);
+	//writeln(blackbody_spec(500e-9, 4000));
 }
